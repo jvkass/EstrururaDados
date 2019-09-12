72f84ca178e984e7209c48b5191761f203e11bae
João Vitor Coelho Nunes da Mata <e80180810@mpce.mp.br>
2019-09-12 13:30:22 -0300

    'Fila'

--- Fila/fila.hpp
+++ Fila/fila.hpp
@@ -4,32 +4,32 @@ using std::nothrow;
 template <typename T>
 struct Fila
 {
-    T *v;
-    T *w;
-    int inicio, fim, tam_v = 2, tam_w;
+	T* v;
+	T* w;
+	int inicio, fim, tam_v = 2, tam_w;
 
-    bool inicializar()
-    {
-        v = new (nothrow) T[tam_v];
-        if (v == nullptr)
-            return true;
-        inicio = -1;
-        fim = 0;
-        //tam_v = 1;
-        return false;
-    }
+	bool inicializar()
+	{
+		v = new (nothrow) T[tam_v];
+		if (v == nullptr)
+			return true;
+		inicio = -1;
+		fim = 0;
+		//tam_v = 1;
+		return false;
+	}
 
-    void terminar()
-    {
-        delete[] v;
-    }
-    bool vazia()
-    {
-        return (inicio == -1);
-    }
-    //redimensionar fica aqui.
-    bool enfilar(T e)
-    {
+	void terminar()
+	{
+		delete[] v;
+	}
+	bool vazia()
+	{
+		return (inicio == -1);
+	}
+	//redimensionar fica aqui.
+	bool enfilar(T e)
+	{
 		if (fim == inicio || (fim == tam_v && inicio == 0))//Cheia 
 		{
 			return true;
@@ -48,67 +48,70 @@ struct Fila
 			**/
 			return vazia();
 		}
-		
-     
-        
-        if (fim == tam_v) fim = 0;
-        
-        v[fim] = e;
-        ++fim;
-        
-        
 
-        return false;
-    }
+
+
+		if (fim == tam_v) fim = 0;
+
+		v[fim] = e;
+		++fim;
+
+
+
+		return false;
+	}
 
 	T printar(bool Index) { //return v[inicio]; 
-    if (Index == true){    return inicio;}
+		if (Index == true) { return inicio; }
 
-    if (Index == false){ return fim;}
+		if (Index == false) { return fim; }
 
-    }
+	}
 
-    // Pilha
-    
-    // Pré-Condição: Pilha não vazia.
-    bool desenfilar()
-    {
-    
+	// Pilha
+
+	// Pré-Condição: Pilha não vazia.
+	bool desenfilar()
+	{
+
 		if (inicio != fim) {
 			++inicio;
 		}
-		if (inicio == tam_v && inicio == fim ) {
+		if (inicio == tam_v && inicio == fim) {
 			inicio = -1;
 			fim = 0;
 		}
+		if (inicio == tam_v && fim != 0) {
+			inicio = 0;
+		}
 
-		
-        if (!vazia() && 4 * (fim + 1) <= tam_v)
-        {
-           /* if (redimensionar(tam_v / 2))
-            {
-                return true;
-            }**/
-        }
-        return false;
-    }
 
-    //Pilha
-    //Pré-condição.: ult+1<= tam_w
+		if (!vazia() && 4 * (fim + 1) <= tam_v)
+		{
+			/* if (redimensionar(tam_v / 2))
+			 {
+				 return true;
+			 }**/
+		}
+		return false;
+	}
+
+	//Pilha
+	//Pré-condição.: ult+1<= tam_w
    /* bool redimensionar(int tam_w)
-    {
-        T *w = new (nothrow) T[tam_w];
-        if (w == nullptr)
-            return true;
-        for (int i = 0; i <= fim; i++)
-        {
-            w[i] = v[i];
-        }
-        delete[] v;
-        v = w;
-        tam_v = tam_w;
-        return false;
-    }
-    **/
+	{
+		T *w = new (nothrow) T[tam_w];
+		if (w == nullptr)
+			return true;
+		for (int i = 0; i <= fim; i++)
+		{
+			w[i] = v[i];
+		}
+		delete[] v;
+		v = w;
+		tam_v = tam_w;
+		return false;
+	}
+	**/
 };  // Pilha
-    //Guardas de inclusão
+	//Guardas de inclusão
--- Fila/main.cpp
+++ Fila/main.cpp
@@ -7,67 +7,68 @@ using std::endl;
 
 int main()
 {
-    Fila<double> F;
-    if (F.inicializar())
-        return 1;
-    cout << "Número de pessoas a serem enfiladas ";
+	Fila<double> F;
+	if (F.inicializar())
+		return 1;
+	cout << "Número de pessoas a serem enfiladas ";
 
-    int i;
-    cin >> i;
-    for (int j = 0; j < i; j++)
-    {
-        cout << "Número para empilhar "
-             << "(Negativo para terminar): ";
+	int i;
+	cin >> i;
+	for (int j = 0; j < i; j++)
+	{
+		cout << "Número para empilhar "
+			<< "(Negativo para terminar): ";
 
-        int num;
-        cin >> num;
+		int num;
+		cin >> num;
 
-        if (F.enfilar(num))
-        {
-            cout << "Fila cheia" << endl;
-        }
-    }
+		if (F.enfilar(num))
+		{
+			cout << "Fila cheia" << endl;
+		}
+	}
 
-    int escolha;
-   
-    while (escolha != -1)
-    {
-        cout <<"Informe a operação desejada (1 - Enfilar; 2 - Desenfilar; 0 - Sair do menu): " << endl;
-         cin >> escolha;
-        if (escolha == 1)
-        {
-            if (F.enfilar(5))
-            {
-                cout << F.printar(true)<<" "<<F.printar(false) << endl;
-                cout << "Fila cheia" << endl;
-            }else { cout << F.printar(true)<<" "<<F.printar(false) << endl;}
-        }
-        if (escolha == 2)
-        {
-          
-            F.desenfilar();
-              cout << F.printar(true)<<" "<<F.printar(false) << endl;
-        }
-        if (escolha == 0){
-            escolha =-1;
-        }
-    }
+	int escolha = 0;
 
-  //  cout << F.printar() << endl;
-  //  F.desenfilar();
-  //  cout << F.printar() << endl;
-  //  F.desenfilar();
-  //  cout << F.printar() << endl;
-  //  if (F.enfilar(5))
-  //  {
-  //      cout << "Fila cheia" << endl;
-  //  }
-  //  cout << F.printar() << endl;
+	while (escolha != -1)
+	{
+		cout << "Informe a operação desejada (1 - Enfilar; 2 - Desenfilar; 0 - Sair do menu): " << endl;
+		cin >> escolha;
+		if (escolha == 1)
+		{
+			if (F.enfilar(5))
+			{
+				cout << F.printar(true) << " " << F.printar(false) << endl;
+				cout << "Fila cheia" << endl;
+			}
+			else { cout << F.printar(true) << " " << F.printar(false) << endl; }
+		}
+		if (escolha == 2)
+		{
 
-    //while (!F.vazia())
-    //{
-    //    cout << ' ' << F.topo();
-    //    if (P.desempilhar())
-    //        return 3;
-    //}
+			F.desenfilar();
+			cout << F.printar(true) << " " << F.printar(false) << endl;
+		}
+		if (escolha == 0) {
+			escolha = -1;
+		}
+	}
+
+	//  cout << F.printar() << endl;
+	//  F.desenfilar();
+	//  cout << F.printar() << endl;
+	//  F.desenfilar();
+	//  cout << F.printar() << endl;
+	//  if (F.enfilar(5))
+	//  {
+	//      cout << "Fila cheia" << endl;
+	//  }
+	//  cout << F.printar() << endl;
+
+	  //while (!F.vazia())
+	  //{
+	  //    cout << ' ' << F.topo();
+	  //    if (P.desempilhar())
+	  //        return 3;
+	  //}
 }
